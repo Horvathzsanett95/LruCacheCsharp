9d68599d0278397d9275c85aa6e6560f27ac8776
Zsani <horvathzsanett.95@gmail.com>
2021-02-20 13:45:14 +0100

    Finished LruCache implementation

---
+++ LruCache.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheOperations
+{
+    public class LruCache<TKey, TValue>
+    {
+        private readonly int _maxCount;
+        private LinkedList<KeyValuePair<TKey, TValue>> _cacheList = new LinkedList<KeyValuePair<TKey, TValue>>();
+        private Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> _cacheMap = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>();
+        public LruCache(int maxCount)
+        {
+            _maxCount = maxCount;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (_cacheMap.Count >= _maxCount)
+            {
+                var nodeToRemove = _cacheList.First;
+                _cacheList.RemoveFirst();
+                _cacheMap.Remove(nodeToRemove.Value.Key);
+            }
+            var keyValuePair = new KeyValuePair<TKey, TValue>(key, value);
+            var node = new LinkedListNode<KeyValuePair<TKey, TValue>>(keyValuePair);
+            _cacheList.AddLast(node);
+            _cacheMap.Add(key, node);
+        }
+
+        public TValue Get(TKey key)
+        {
+            TValue value = default(TValue);
+            try
+            {
+                LinkedListNode<KeyValuePair<TKey, TValue>> node;
+                if (_cacheMap.TryGetValue(key, out node))
+                {
+                    value = node.Value.Value;
+                    _cacheList.Remove(node);
+                    _cacheList.AddLast(node);
+                }
+            }
+            catch (AccessViolationException exception)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", exception.Message);
+            }
+            return value;
+        }
+    }
+
+
+}
